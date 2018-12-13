4ef29185a97b2fe0fd87061ac3809f4ec5ca9a64
Conti Davide <contidavide971@gmail.com>
2018-12-13 21:43:52 +0100

    creation of the static part of the navbar and add css

---
+++ FoodCampus/php/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-custom">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Ricerca prodotti</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Ricerca fornitori</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+        </ul>
+    </div>
+    <form class="form-inline" action="#">
+        <div class="input-group abs-center-x">
+            <input class="form-control" type="text" placeholder="Search">
+            <div class="input-group-prepend">
+                <button class="btn btn-default fas fa-search" type="submit"></button>
+            </div>
+        </div>
+    </form>
+    <ul class="navbar-nav">
+        <li class="nav-item"><span class="badge badge-light">0</span><a class="nav-link fas fa-shopping-cart" href="#"></a></li>
+    </ul>
+</nav>
