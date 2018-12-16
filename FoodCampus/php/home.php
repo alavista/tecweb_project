d0d02427295e7c683e79bd1438263a795e367b7f
Conti Davide <contidavide971@gmail.com>
2018-12-16 12:01:34 +0100

    home.php: add slideshow

--- FoodCampus/php/home.php
+++ FoodCampus/php/home.php
@@ -15,9 +15,51 @@
          <!--Font awesome-->
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
          <link rel="stylesheet" type="text/css" title="stylesheet" href="../css/navbar.css">
+         <link rel="stylesheet" type="text/css" title="stylesheet" href="../css/home.css">
     </head>
     <body>
           <?php require_once 'navbar.php';?>
-          AA
-    </body>
+          <div id="foodCategories" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ul class="carousel-indicators">
+              <li data-target="#foodCategories" data-slide-to="0" class="active"></li>
+              <li data-target="#foodCategories" data-slide-to="1"></li>
+              <li data-target="#foodCategories" data-slide-to="2"></li>
+              <li data-target="#foodCategories" data-slide-to="3"></li>
+          </ul>
+          <!-- The slideshow -->
+          <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <a href="#"><img src="../res/pizza.jpg" alt="Pizza"></a>
+                  <div class="carousel-caption">
+                      <h3>Pizze</h3>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <a href="#"><img src="../res/panino.jpg" alt="Panini"></a>
+                  <div class="carousel-caption">
+                      <h3>Panini</h3>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <a href="#"><img src="../res/primo.jpg" alt="Primi"></a>
+                  <div class="carousel-caption">
+                      <h3>Primi</h3>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <a href="#"><img src="../res/secondo.jpg" alt="Secondi"></a>
+                  <div class="carousel-caption">
+                      <h3>Secondi</h3>
+                  </div>
+              </div>
+              <!-- Left and right controls -->
+              <a class="carousel-control-prev" href="#foodCategories" data-slide="prev">
+                  <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a class="carousel-control-next" href="#foodCategories" data-slide="next">
+                  <span class="carousel-control-next-icon"></span>
+              </a>
+          </div>
+      </body>
 </html>
