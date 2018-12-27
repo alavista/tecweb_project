3a3131084655d24b6c97f015339713723b37c677
Conti Davide <contidavide971@gmail.com>
2018-12-27 18:12:32 +0100

    supplier.php: fix navbar path

--- FoodCampus/php/suppliers/supplier.php
+++ FoodCampus/php/suppliers/supplier.php
@@ -21,7 +21,7 @@
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
          <!--CSS-->
-         <link rel="stylesheet" type="text/css" title="stylesheet" href="../..css/navbar.css">
+         <link rel="stylesheet" type="text/css" title="stylesheet" href="../../css/navbar.css">
          <link rel="stylesheet" type="text/css" title="stylesheet" href="supplier.css">
     </head>
     <body>
