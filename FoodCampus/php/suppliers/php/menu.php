a78ea91de1238a016fa6c18e57258af498eb83ca
Conti Davide <contidavide971@gmail.com>
2019-01-04 17:47:22 +0100

    add the display 'vegano', 'celiaco' and 'surgelato' of the products in the menu

--- FoodCampus/php/suppliers/php/menu.php
+++ FoodCampus/php/suppliers/php/menu.php
@@ -120,7 +120,7 @@
                                     ?>
                                     <h4><?php echo ucwords($res1->fetch_assoc()["nome"]); ?></h4>
                                     <?php
-                                    $query="SELECT nome, costo FROM prodotto WHERE IDFornitore = ? AND IDCategoria = ?";
+                                    $query="SELECT * FROM prodotto WHERE IDFornitore = ? AND IDCategoria = ?";
                                     if ($stmt = $conn->prepare($query)) {
                                         $stmt->bind_param("ss", $idSupplier, $idCategory);
                                         if ($stmt->execute()) {
@@ -128,9 +128,30 @@
                                             if ($res2->num_rows > 0) {
                                                 while ($product = $res2->fetch_assoc()) {
                                                 ?>
-                                                    <div class="product">
-                                                        <span class="float-left"><?php echo $product["nome"]; ?></span>
-                                                        <span class="float-right">
+                                                    <div class="row product">
+                                                        <span class="col-md-9">
+                                                            <?php
+                                                            echo $product["nome"];
+                                                            if ($product["vegano"] || $product["celiaco"] || $product["surgelato"]) {
+                                                                ?>
+                                                                <span class="font-italic">
+                                                                    <?php
+                                                                    if ($product["vegano"]) {
+                                                                        echo " (vegano)";
+                                                                    }
+                                                                    if ($product["celiaco"]) {
+                                                                        echo " (celiaco)";
+                                                                    }
+                                                                    if ($product["surgelato"]) {
+                                                                        echo " (surgelato)";
+                                                                    }
+                                                                    ?>
+                                                                </span>
+                                                                <?php
+                                                            }
+                                                            ?>
+                                                        </span>
+                                                        <span class="col-md-3">
                                                             <span><?php echo $product["costo"]; ?> €</span>
                                                             <span <?php if ($isSupplier) { echo "data-toggle='popover' data-trigger='hover' data-content='I fornitori non possono acquistare'"; } ?>>
                                                                 <button type="button" class="btn btn-deafult btn-kart" <?php if ($isSupplier) { echo "style='pointer-events: none;' disabled"; } ?>><i class="fas fa-cart-plus"></i></button>
