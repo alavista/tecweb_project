e1c3ee5cbd2b18e2232dc6de3ca010a52365ce53
Conti Davide <contidavide971@gmail.com>
2019-01-06 20:57:52 +0100

    improve

--- FoodCampus/php/navbar/navbar.php
+++ FoodCampus/php/navbar/navbar.php
@@ -15,7 +15,7 @@ function computeNumberNotification($conn, $field, $userId) {
             }
         }
     }
-    return -1;
+    return 0;
 }
 
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
