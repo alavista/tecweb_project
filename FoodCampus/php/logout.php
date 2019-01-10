c45470b9236ba8f69f8679b2da29ace23f348383
Conti Davide <contidavide971@gmail.com>
2019-01-10 12:23:12 +0100

    fix unset cookies

--- FoodCampus/php/logout.php
+++ FoodCampus/php/logout.php
@@ -13,7 +13,16 @@
     // Recupera i parametri di sessione.
     $params = session_get_cookie_params();
     // Cancella i cookie attuali.
-    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    if (isset($_SERVER['HTTP_COOKIE'])) {
+        $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+        foreach($cookies as $cookie) {
+            $parts = explode('=', $cookie);
+            $name = trim($parts[0]);
+            setcookie($name, '', time() - (86400 * 365 * 5));
+            setcookie($name, '', time() - (86400 * 365 * 5), '/');
+        }
+    }
+
     // Cancella la sessione.
     session_destroy();
     header("Location: $actual_link");
